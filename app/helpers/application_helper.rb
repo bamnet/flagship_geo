34c6ed9d200c17df0edcc0ce2b1d88260e61fe3e
Brian M <michab3@rpi.edu>
2010-06-01 11:53:58 -0400

    Display a mini-map on point new/edit/show pages so you can view points and easily move them around.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # Generates html for a div for use as a map canvas.
+  # Defaults to a small div that works well for showing 1 or 2 points 
+  # that are in close proximity to each other.
+  def map_canvas(id = "map_canvas", width = "250px", height = "250px")
+    tag("div", {:id => id, :style => "width: #{width}; height: #{height};"}, true) + raw("</div>")
+  end
 end
