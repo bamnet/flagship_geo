e08cad1ecc5039059494d7bbd0ead7a23de3adaa
Brian M <michab3@rpi.edu>
2010-06-21 13:03:07 -0400

    Introducing cartesian coordinates and supporting library (degrees to radians and visa-versa).  Also adding helper to find the geometric midpoint for a series of points.  Tests to come, I promise.

--- app/helpers/points_helper.rb
+++ app/helpers/points_helper.rb
@@ -1,2 +1,22 @@
 module PointsHelper
+
+  # Identify the midpoint of a series of points.
+  # Uses a center of gravity technique, based off the 
+  # cartesian coordinates for each point.
+  # Logic from: http://www.geomidpoint.com/calculation.html
+  def midpoint(points = [])
+    x = y = z = 0
+    points.each do |point|
+      x += point.x
+      y += point.y
+      z += point.z
+    end
+    x, y, z = x/points.length, y/points.length, z/points.length
+    longitude = Math.atan2(y, x).radians
+    hypotenuse = Math.sqrt(x * x + y * y)
+    latitude = Math.atan2(z, hypotenuse).radians
+    
+    [latitude, longitude]
+  end
+
 end
--- app/models/point.rb
+++ app/models/point.rb
@@ -6,4 +6,22 @@ class Point < ActiveRecord::Base
   validates :name, :presence => true
   validates :longitude, :numericality => true, :inclusion => { :in => -180..180 }
   validates :latitude, :numericality => true, :inclusion => { :in => -90..90}
+  
+  # The cartesian x coordinate of a point.
+  def x
+    require 'degrees'
+    Math.cos(latitude.degrees) * Math.cos(longitude.degrees)
+  end
+  
+  # The cartesian y coordinate of a point
+  def y
+    require 'degrees'
+    Math.cos(latitude.degrees) * Math.sin(longitude.degrees)
+  end
+  
+  # The cartesian z coordinate of a point
+  def z
+    require 'degrees'
+    Math.sin(latitude.degrees)
+  end
 end
