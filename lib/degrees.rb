45d7fe2e0e86ac1b7b43b5303ca73abf5379afba
Brian M <michab3@rpi.edu>
2010-06-21 15:05:04 -0400

    Adding some documentation to the degrees/radians conversion.

--- lib/degrees.rb
+++ lib/degrees.rb
@@ -1,7 +1,14 @@
 class Numeric
+  # Convert a degrees unit to radians.
+  #
+  # e.g. 45.degrees => 0.785398163397448
   def degrees
     self * Math::PI / 180 
   end
+  
+  # Convert a radians unit to degrees.
+  #
+  # e.g 0.785398163397448.radians => 45.0
   def radians
     self * 180 / Math::PI
   end
