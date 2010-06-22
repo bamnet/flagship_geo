cf3981d31a2a3613bc4348e241d7c06836a2cb19
Brian M <michab3@rpi.edu>
2010-06-22 15:38:26 -0400

    Refactor javascript for map creation & point display.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,34 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Create a new map, centered around 
+// center_point, with the specified
+// zoomlevel, and put it in the identified
+// div (defaults to "map_canvas")
+function setup_map(center_point, zoom_level, div){
+  div = div || "map_canvas"; //A default value
+  var center = new google.maps.LatLng(center_point.latitude, center_point.longitude);
+  var options = {
+    zoom: zoom_level,
+    center: center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById(div), options);
+  return map;
+}
+
+// Add a point (marker) to a map object.
+// The point should be an object with a
+// name, description, latitude, and longitude.
+function add_point_to_map(point, map){
+  var infowindow = new google.maps.InfoWindow({
+    content: point.description
+  });
+  
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(point.latitude, point.longitude), 
+    map: map,
+    title:point.name
+  });
+  
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
+}
