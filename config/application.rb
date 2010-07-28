a6529e9bc407b76847bc10b9f124ef8fc78ad927
Brian M <michab3@rpi.edu>
2010-07-28 13:37:40 -0400

    Change how we set the :defaults for javascript tags.

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module Geo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # JavaScript files you want as :defaults (application.js is always included).
+    config.action_view.javascript_expansions[:defaults] = ['jquery-1.4.2.min', 'jquery-ujs/src/rails']
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
